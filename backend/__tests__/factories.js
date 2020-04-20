7fe62a008055af858103fbb510b6604706cc3c0e
Felipe Santiago <you@example.com>
2020-04-19 23:08:52 -0300

    feat: add tests with jest on the backend

---
+++ backend/__tests__/factories.js
@@ -0,0 +1,39 @@
+import faker from 'faker';
+import { factory } from 'factory-girl';
+
+import User from '../src/app/models/User';
+import Recipient from '../src/app/models/Recipient';
+import Deliveryman from '../src/app/models/Deliveryman';
+import Delivery from '../src/app/models/Delivery';
+
+factory.define('User', User, {
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+});
+
+factory.define('Recipient', Recipient, {
+  name: faker.name.findName(),
+  street: faker.address.streetName(),
+  number: faker.random.number(999),
+  address_details: faker.address.secondaryAddress(),
+  state: faker.address.state,
+  city: faker.address.city,
+  zip_code: faker.address.zipCode(),
+});
+
+factory.define('Deliveryman', Deliveryman, {
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+});
+
+factory.define('Deliveryman2', Deliveryman, {
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+});
+
+factory.define('Delivery', Delivery, {
+  product: faker.random.word(),
+});
+
+export default factory;
